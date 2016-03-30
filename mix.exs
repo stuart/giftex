53f0b67e49fc7eb17b6df686c8d9d98a74426c61
Stuart Coyle <stuart.coyle@gmail.com>
2016-03-30 21:01:56 +1000

    Add credo and ex_doc

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Giftex.Mixfile do
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps ++ deps(Mix.env)]
   end
 
   def application do
@@ -16,5 +16,14 @@ defmodule Giftex.Mixfile do
 
   defp deps do
     [{:neotoma, "~> 1.7.0"}]
+  end
+
+  defp deps :dev do
+    [{:credo, "~>0.3.10"},
+     {:ex_doc, "~>0.11.4"}]
+  end
+
+  defp deps _ do
+    []
   end
 end
